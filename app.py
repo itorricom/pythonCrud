e0b0be96c10af62b508690a2844f22d42371ace0
Isidoro Torrico Mamani <itorrico.m@gmail.com>
2022-07-27 15:20:06 -0400

    Conexion a base de datos

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
+from re import A
 from flask import Flask
 from flask import render_template
+from flaskext.mysql import MySQL
+
 
 app = Flask(__name__)
 
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'python'
+mysql.init_app(app)
+
 @app.route('/')
 def index():
+    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'Isidoro', 'itorrico.m@gmail.com', 'foto.jpg');"
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+
     return render_template('empleados/index.html')
 
 if __name__ == '__main__':
